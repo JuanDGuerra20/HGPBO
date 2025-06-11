74dd728d59ab2fa82e6bc9412fef2c50b346a8c5
Juan David Guerra <juandavidguerra1@gmail.com>
2025-06-11 12:06:36 -0400

    fixed vanilla script neural

--- vanilla_script.py
+++ vanilla_script.py
@@ -213,18 +213,23 @@ def training_procedure(nbr_query, nbr_repetition, nbr_rand_init, training_iter, 
         y = np.mean(exploitation_scores, axis=0)
         over_exploit.append(y)
 
-        std = np.std(exploitation_scores, axis=0)
+        """std = np.std(exploitation_scores, axis=0)
         plt.plot(y, label='Exploitation')
-        plt.fill_between(range(len(y)), y - std, y + std, alpha=0.4)
+        plt.fill_between(range(len(y)), y - std, y + std, alpha=0.4)"""
         heatmap_data = np.array(heatmap_data)
         heatmap_data = np.reshape(heatmap_data, (-1, nbr_query, len(test_y_hier)))
         r2 = vi.heatmap_r_score(heatmap_data, test_y_hier)
-        plt.plot(r2, label="Parent R2")
+        r2_avg = np.mean(r2, axis=0)
+        r2_std = np.std(r2, axis=0)
+        # r2_std = np.insert(r2_std, 0, np.zeros((2 - len(children)) *nbr_rand_init))[:nbr_query]
+
+        plt.plot(r2_avg, label="Parent R2")
+        plt.fill_between(range(len(r2_std)), r2_avg - r2_std, r2_avg + r2_std, alpha=0.4)
 
         k = str(kappa).replace(".", ",")
 
         plt.legend()
-        plt.ylim(0, 1.1)
+        plt.ylim(-0.1, 1.1)
         plt.title(f'vanilla HGP-BO {nbr_repetition} repetitions with kappa value {k}')
         plt.savefig(
             f'vanilla{folder_of_the_day}/differentiable_plots/vanilla_Neural_HGP-BO_{nbr_repetition}_repetitions_kappa_{k}_query_{nbr_query}')
