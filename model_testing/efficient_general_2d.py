fixed bug and changed standard deviation in plotting to standard error

model_testing/efficient_general_2d.py
@@ -558,7 +558,7 @@ def training_procedure(nbr_query, nbr_repetition, nbr_rand_init, dimension, trai
                 y = np.mean(better_exploration_score, axis=0)
                 y = np.insert(y, 0, np.zeros((2 - len(children))*nbr_rand_init))[:nbr_query]
                 over_explor.append(y)
-                std = np.std(better_exploration_score, axis=0)
+                std = np.std(better_exploration_score, axis=0) / np.sqrt(len(better_exploration_score))
                 std = np.insert(std, 0, np.zeros((2 - len(children))*nbr_rand_init))[:nbr_query]
                 plt.plot(y, label='Exploration')
                 plt.fill_between(range(len(y)), y - std, y + std, alpha=0.4)
@@ -571,7 +571,7 @@ def training_procedure(nbr_query, nbr_repetition, nbr_rand_init, dimension, trai
                 r2 = vi.heatmap_r_score(heatmap_data, y_hier)
                 r2 = np.insert(r2, 0, np.zeros(((2 - len(children)) *nbr_rand_init, 1)), axis=1)[:, :nbr_query]
                 r2_avg = np.mean(r2, axis=0)
-                r2_std = np.std(r2, axis=0)
+                r2_std = np.std(r2, axis=0) / np.sqrt(len(r2))
                 #r2_std = np.insert(r2_std, 0, np.zeros((2 - len(children)) *nbr_rand_init))[:nbr_query]
 
                 plt.plot(r2_avg, label="Parent R2")
@@ -581,7 +581,7 @@ def training_procedure(nbr_query, nbr_repetition, nbr_rand_init, dimension, trai
                 all_children = np.insert(all_children, 0, np.zeros(((2 - len(children))*nbr_rand_init, 1)), 1)[:, :nbr_query]
 
                 avg_child = np.mean(all_children, axis=0)
-                std_child = np.std(all_children, axis=0)
+                std_child = np.std(all_children, axis=0) / np.sqrt(len(all_children))
 
                 plt.plot(avg_child, label='Child Avg R2')
                 plt.fill_between(range(len(avg_child)), avg_child - std_child, avg_child + std_child, alpha=0.4)

model_testing/laferriere_script_synthetic.py
@@ -411,22 +411,22 @@ def training_procedure(nbr_query, nbr_repetition, nbr_rand_init, dimension, trai
                            f'{data_name}/{model_name.lower()}{folder_of_the_day}/models/kappa_{k}_gamma_{g}_nu_{n}_model_state_{nbr_query}_queries_eps_{e}_init_{nbr_rand_init}_train_iter_{training_iter}.pth')
 
                 y = np.mean(better_exploration_score, axis=0)
-                y = np.insert(y, 0, np.zeros(2 * nbr_rand_init))[:nbr_query]
+                y = np.insert(y, 0, np.zeros((2 - len(children)) * nbr_rand_init))[:nbr_query]
                 over_explor.append(y)
-                std = np.std(better_exploration_score, axis=0)
-                std = np.insert(std, 0, np.zeros(2 * nbr_rand_init))[:nbr_query]
+                std = np.std(better_exploration_score, axis=0) / np.sqrt(len(better_exploration_score))
+                std = np.insert(std, 0, np.zeros((2 - len(children)) * nbr_rand_init))[:nbr_query]
                 plt.plot(y, label='Exploration')
                 plt.fill_between(range(len(y)), y - std, y + std, alpha=0.4)
 
                 y = np.mean(better_exploitation_score, axis=0)
-                y = np.insert(y, 0, np.zeros(2 * nbr_rand_init))[:nbr_query]
+                y = np.insert(y, 0, np.zeros((2 - len(children)) * nbr_rand_init))[:nbr_query]
 
                 over_exploit.append(y)
 
                 r2 = vi.heatmap_r_score(heatmap_data, y_hier)
                 r2 = np.insert(r2, 0, np.zeros(((2 - len(children)) * nbr_rand_init, 1)), axis=1)[:, :nbr_query]
                 r2_avg = np.mean(r2, axis=0)
-                r2_std = np.std(r2, axis=0)
+                r2_std = np.std(r2, axis=0) / np.sqrt(len(r2))
                 # r2_std = np.insert(r2_std, 0, np.zeros((2 - len(children)) *nbr_rand_init))[:nbr_query]
 
                 plt.plot(r2_avg, label="Parent R2")
@@ -437,7 +437,7 @@ def training_procedure(nbr_query, nbr_repetition, nbr_rand_init, dimension, trai
                                :nbr_query]
 
                 avg_child = np.mean(all_children, axis=0)
-                std_child = np.std(all_children, axis=0)
+                std_child = np.std(all_children, axis=0) / np.sqrt(len(all_children))
 
                 plt.plot(avg_child, label='Child Avg R2')
                 plt.fill_between(range(len(avg_child)), avg_child - std_child, avg_child + std_child, alpha=0.4)

model_testing/synthetic_models.py
@@ -180,8 +180,8 @@ def get_next_query_value(next_query_pins, X, Y, nbr_rdm_points_data=20, noise=0)
     new_query_value_mean = np.mean(new_training_values)
     new_query_value_random = np.random.choice(new_training_values)
 
-    new_query_value_mean += np.random.normal(0, noise, size=new_query_value_mean.shape) * (torch.max(reshape_y)-torch.min(reshape_y))
-    new_query_value_random += np.random.normal(0, noise, size=new_query_value_random.shape) * (torch.max(reshape_y)-torch.min(reshape_y))
+    new_query_value_mean += np.random.normal(0, noise, size=new_query_value_mean.shape) * (np.max(reshape_y.numpy())-np.min(reshape_y.numpy()))
+    new_query_value_random += np.random.normal(0, noise, size=new_query_value_random.shape) * (np.max(reshape_y.numpy())-np.min(reshape_y.numpy()))
 
     return new_query_value_random, new_query_value_mean
 

vanilla_script.py
@@ -206,7 +206,7 @@ def training_procedure(nbr_query, nbr_repetition, nbr_rand_init, training_iter, 
 
         y = np.mean(exploration_scores, axis=0)
         over_explor.append(y)
-        std = np.std(exploration_scores, axis=0)
+        std = np.std(exploration_scores, axis=0) / np.sqrt(len(exploration_scores))
         plt.plot(y, label='Exploration')
         plt.fill_between(range(len(y)), y - std, y + std, alpha=0.4)
 
@@ -220,7 +220,7 @@ def training_procedure(nbr_query, nbr_repetition, nbr_rand_init, training_iter, 
         heatmap_data = np.reshape(heatmap_data, (-1, nbr_query, len(test_y_hier)))
         r2 = vi.heatmap_r_score(heatmap_data, test_y_hier)
         r2_avg = np.mean(r2, axis=0)
-        r2_std = np.std(r2, axis=0)
+        r2_std = np.std(r2, axis=0) / np.sqrt(len(r2))
         # r2_std = np.insert(r2_std, 0, np.zeros((2 - len(children)) *nbr_rand_init))[:nbr_query]
 
         plt.plot(r2_avg, label="Parent R2")